626fe16332b0b94b7f8284dde16c759616091691
João Vitor Moreira Passos <moreirapassosj@gmail.com>
2023-02-04 20:53:05 -0300

    up

--- projeto/settings.py
+++ projeto/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "receitas.apps.ReceitasConfig"
+    "receitas.apps.ReceitasConfig",
 ]
 
 MIDDLEWARE = [
@@ -119,7 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static_base",
+]
+# STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
