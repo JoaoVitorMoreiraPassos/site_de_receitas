dfdc9b7b26d77ac25c0b02a3cd8bef7b7ca62da3
João Vitor Moreira Passos <moreirapassosj@gmail.com>
2023-02-05 14:43:38 -0300

    colocando atribuos aleatorios nas receitas, utilizando heranças, context para enviar dados para o html  e ajustando o estilo dos cards de receita

--- receitas/views.py
+++ receitas/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.receitas.factory import make_recipe
 
 # Create your views here.
+
+
 def home(request):
-    return render(request, "receitas/pages/home.html")
+    return render(request, "receitas/pages/home.html", context={
+            "receitas": [make_recipe() for _ in range(10)]
+        }
+    )
+
 
 def receitas(request, id):
-    return render(request, "receitas/pages/receitas-view.html")
+    return render(request, "receitas/pages/receitas-view.html", context={
+            "receita": make_recipe(),
+            "is_detail_page": True
+        }
+    )
