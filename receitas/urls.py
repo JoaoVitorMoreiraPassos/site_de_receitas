ca4be3168509b6680170719ba93a200e14028103
João Vitor Moreira Passos <moreirapassosj@gmail.com>
2023-02-19 18:18:44 -0300

    Usando o model Recipe real (com QuerySet) na view home

--- receitas/urls.py
+++ receitas/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = "receitas"
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("receitas/<int:id>/", views.receitas, name="receitas")
+    path("receitas/category/<int:category_id>/",
+         views.category,
+         name="category"),
+    path("receitas/<int:id>/", views.receitas, name="receitas"),
 ]
--- receitas/views.py
+++ receitas/views.py
@@ -1,20 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from utils.receitas.factory import make_recipe
-
+from .models import Receita
 # Create your views here.
+
+app_name = "receitas"
 
 
 def home(request):
+    receitas = Receita.objects.all().order_by("-id")
+    print("receitas: ", receitas)
     return render(request, "receitas/pages/home.html", context={
-            "receitas": [make_recipe() for _ in range(10)]
-        }
+        "receitas": receitas
+    }
+    )
+
+
+def category(request, category_id):
+    receitas = Receita.objects.filter(
+        category__id=category_id
+    ).order_by("-id")
+    print("receitas: ", receitas)
+    return render(request, "receitas/pages/home.html", context={
+        "receitas": receitas
+    }
     )
 
 
 def receitas(request, id):
     return render(request, "receitas/pages/receitas-view.html", context={
-            "receita": make_recipe(),
-            "is_detail_page": True
-        }
+        "receita": make_recipe(),
+        "is_detail_page": True
+    }
     )
